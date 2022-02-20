fe75cc349c549dbc809cfe1b94d9a368cdd3f6f3
Lana Kalashnyk <lanakalashnyk@Lanas-MacBook-Air.local>
2022-02-20 02:15:18 -0700

    changed CSS

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from 'react';
 import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
-import Token from './artifacts/contracts/Token.sol/Token.json'
+import CouchToken from './artifacts/contracts/Token.sol/CouchToken.json'
 
 const greeterAddress = "your-contract-address"
 const tokenAddress = "your-contract-address"
@@ -16,19 +16,6 @@ function App() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   }
 
-  async function fetchGreeting() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      console.log({ provider })
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-    }    
-  }
 
   async function getBalance() {
     if (typeof window.ethereum !== 'undefined') {
@@ -40,21 +27,7 @@ function App() {
     }
   }
 
-  async function setGreeting() {
-    if (!greeting) return
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      console.log({ provider })
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(greeting)
-      await transaction.wait()
-      fetchGreeting()
-    }
-  }
-
-  async function sendCoins() {
+  async function sendCouch() {
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -62,20 +35,15 @@ function App() {
       const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
       const transation = await contract.transfer(userAccount, amount);
       await transation.wait();
-      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+      console.log(`${amount} Couch successfully sent to ${userAccount}`);
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={fetchGreeting}>Fetch Greeting</button>
-        <button onClick={setGreeting}>Set Greeting</button>
-        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
-
-        <br />
-        <button onClick={getBalance}>Get Balance</button>
-        <button onClick={sendCoins}>Send Coins</button>
+        <button onClick={getBalance}>Get Couch</button>
+        <button onClick={sendCoins}>Send Couch</button>
         <input onChange={e => setUserAccount(e.target.value)} placeholder="Account ID" />
         <input onChange={e => setAmount(e.target.value)} placeholder="Amount" />
       </header>
